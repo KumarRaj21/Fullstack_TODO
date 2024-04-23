f4b8b58de6b17230ec4246a51a158e00fe6c1dc9
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-04-23 17:02:18 +0530

    Added Navbar

---
+++ src/Configurations/NavRoutes.js
@@ -0,0 +1,15 @@
+const NAV_ROUTES=[
+    {
+        text:"All tasks",
+        path:"/all"
+    },
+    {
+        text:"In Progress",
+        path:"/inprogress"
+    },
+    {
+        text:"Done",
+        path:"/done"
+    }
+]
+export default NAV_ROUTES;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 import "./Navbar.css";
+import NAV_ROUTES from "../../Configurations/NavRoutes";
+import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
-    <div className='Navbar'>
-      
+    <div className="Navbar">
+      <div className="Task">
+        <h2>Tasks</h2>
+        <button>+</button>
+      </div>
+      <div className="navigations">
+        {
+          NAV_ROUTES.map((item,index)=>{
+            return <NavLink to={item.path} key={index}>{item.text}</NavLink>
+          })
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
